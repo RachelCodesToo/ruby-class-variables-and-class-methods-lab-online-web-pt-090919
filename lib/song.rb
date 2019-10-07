921d7b3379ad7c1850171eb4a32709e95218ce00
Anne Fausto <a.r.fausto@gmail.com>
2019-10-07 22:14:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,13 @@ class Song
   @@count = 0 
   @@artists = []
   @@genres = []
-  binding.pry
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+  end 
+  
 
 end 
     
